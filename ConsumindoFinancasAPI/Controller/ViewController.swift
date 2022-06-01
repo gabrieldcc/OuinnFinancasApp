2fc45aabb4d5aaa0783ac01d6874964402ba8fd5
Gabriel de Castro Chaves <gabrieldecastrochaves@MacBook-Air-de-Gabriel.local>
2022-06-01 17:28:09 -0300

    inserindo informacoes na label ibovespa e selic

--- ConsumindoFinancasAPI/Controller/ViewController.swift
+++ ConsumindoFinancasAPI/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    
     //MARK: Life Cycle View
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,16 @@ class ViewController: UIViewController {
 
         DolarAPI().makeRequest { (objetoDecodificado) in
             self.dolarLabel.text = "Dólar: \(objetoDecodificado.USDBRL.ask)"
+            self.ibovespaLabel.text = "Ibovespa: \( self.ibovespa)"
+            self.selicLabel.text = "Selic: \( self.selic)"
         }
-        
-
     }
+    
+    //MARK: Atributos
+    
+    let ibovespa = "112.01"
+    let selic = "12.05%"
+    
     
     //MARK: IBOutlets
     
@@ -32,7 +39,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var infoView: UIView!
     @IBOutlet weak var dolarLabel: UILabel!
-
+    @IBOutlet weak var ibovespaLabel: UILabel!
+    @IBOutlet weak var selicLabel: UILabel!
     
         
 
